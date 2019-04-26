0bc27414a014e19a0bb448d73e90a2ebbfbe02f9
Henry Wallace <h@henrywallace.io>
2019-04-25 21:52:26 -0400

    move NaiveIndex to own index mod
    And the fallout of this is that we add len and get methods to the
    NaiveIndex struct.

---
+++ src/index.rs
@@ -0,0 +1,79 @@
+use crate::distance;
+use lexical;
+use num_traits::ToPrimitive;
+use ordered_float::OrderedFloat;
+use std::collections::{BinaryHeap, HashMap};
+use std::error::Error;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+type Result<T> = std::result::Result<T, Box<dyn Error>>;
+
+pub struct NaiveIndex {
+    vecs: HashMap<String, Vec<f32>>,
+}
+
+// TODO: Learn how to write proper rust flavored docs.
+// Can we make clippy more angry for eliding these docs?
+//
+// TODO: Define a trait shared across different LSH methods.
+impl NaiveIndex {
+    pub fn from_path(path: &str, skip: usize) -> Result<NaiveIndex> {
+        let mut idx = NaiveIndex {
+            vecs: HashMap::new(),
+        };
+
+        let f = File::open(path)?;
+        for (i, line) in BufReader::new(f).lines().enumerate() {
+            if i < skip {
+                continue;
+            }
+            let mut word: Option<String> = None;
+            let mut vec = vec![];
+            for (i, part) in line?.split_whitespace().enumerate() {
+                if i == 0 {
+                    word = Some(part.to_owned());
+                    continue;
+                }
+                let x: f32 = lexical::try_parse(part)?;
+                vec.push(x.to_owned());
+            }
+            match word {
+                Some(word) => {
+                    idx.vecs.insert(word, vec.to_owned());
+                }
+                None => println!("empty line: {}", i),
+            }
+        }
+
+        Ok(idx)
+    }
+
+    pub fn get(&self, key: &str) -> Option<Vec<f32>> {
+        match self.vecs.get(key) {
+            Some(v) => Some(v.clone()),
+            None => None,
+        }
+    }
+
+    pub fn len(&self) -> usize {
+        self.vecs.len()
+    }
+
+    pub fn similar(&self, query: &[f32], k: u8) -> Vec<(String, f32)> {
+        let mut heap = BinaryHeap::new();
+        for (word, vec) in self.vecs.iter() {
+            let d = distance::pnorm(2.0, query, vec);
+            let item = (OrderedFloat(d), word);
+            heap.push(item);
+            if heap.len() as u8 > k {
+                heap.pop();
+            }
+        }
+        let mut sim = vec![];
+        for (d, word) in heap.into_sorted_vec() {
+            sim.push((word.to_owned(), d.to_f32().unwrap()));
+        }
+        sim
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,75 +1,12 @@
 use clap::{crate_authors, crate_version, App, Arg, SubCommand};
 
-use lexical;
-use num_traits::ToPrimitive;
-use ordered_float::OrderedFloat;
-use std::collections::{BinaryHeap, HashMap};
 use std::error::Error;
-use std::fs::File;
-use std::io::{BufRead, BufReader};
 
 mod distance;
 mod hash;
+mod index;
 
 type Result<T> = std::result::Result<T, Box<dyn Error>>;
-
-struct NaiveIndex {
-    vecs: HashMap<String, Vec<f32>>,
-}
-
-// TODO: Learn how to write proper rust flavored docs.
-// Can we make clippy more angry for eliding these docs?
-//
-// TODO: Define a trait shared across different LSH methods.
-impl NaiveIndex {
-    fn from_path(path: &str, skip: usize) -> Result<NaiveIndex> {
-        let mut idx = NaiveIndex {
-            vecs: HashMap::new(),
-        };
-
-        let f = File::open(path)?;
-        for (i, line) in BufReader::new(f).lines().enumerate() {
-            if i < skip {
-                continue;
-            }
-            let mut word: Option<String> = None;
-            let mut vec = vec![];
-            for (i, part) in line?.split_whitespace().enumerate() {
-                if i == 0 {
-                    word = Some(part.to_owned());
-                    continue;
-                }
-                let x: f32 = lexical::try_parse(part)?;
-                vec.push(x.to_owned());
-            }
-            match word {
-                Some(word) => {
-                    idx.vecs.insert(word, vec.to_owned());
-                }
-                None => println!("empty line: {}", i),
-            }
-        }
-
-        Ok(idx)
-    }
-
-    fn similar(&self, query: &[f32], k: u8) -> Vec<(String, f32)> {
-        let mut heap = BinaryHeap::new();
-        for (word, vec) in self.vecs.iter() {
-            let d = distance::pnorm(2.0, query, vec);
-            let item = (OrderedFloat(d), word);
-            heap.push(item);
-            if heap.len() as u8 > k {
-                heap.pop();
-            }
-        }
-        let mut sim = vec![];
-        for (d, word) in heap.into_sorted_vec() {
-            sim.push((word.to_owned(), d.to_f32().unwrap()));
-        }
-        sim
-    }
-}
 
 fn main() -> Result<()> {
     let matches = App::new("blush")
@@ -91,10 +28,10 @@ fn main() -> Result<()> {
             Some("naive") => {
                 // TODO: We should really have a separate parser type. For instance glove,
                 // fasttext, generic text embedding file, or binary embedding file.
-                let idx = NaiveIndex::from_path(data, 1)?;
-                dbg!(idx.vecs.len());
-                let vec = &idx.vecs[key];
-                dbg!(idx.similar(vec, 8));
+                let idx = index::NaiveIndex::from_path(data, 1)?;
+                dbg!(idx.len());
+                let vec = idx.get(key).unwrap();
+                dbg!(idx.similar(vec.as_slice(), 8));
             }
             // TODO: What's a better way to return CLI errors here?
             Some(index) => println!("unsupported index: {}", index),
